f44e03418f22a66638e68aa044a542efdefb7224
federicomedinabyu <med19006@byui.edu>
2021-03-18 21:47:16 -0600

    ok

--- lesson10/js/apiwheater.js
+++ lesson10/js/apiwheater.js
@@ -10,17 +10,35 @@ fetch(apiURL)
    document.getElementById('humidity').innerHTML = town.main.humidity;
    document.getElementById('windspeed').innerHTML = Math.round(town.wind.speed);
  
-   //windchill//
-   let output = "N/A"
-        if (temp <= 50 && wind > 3) {
-            output = (35.74 + 0.6215 * temp - 35.75 * wind ** 0.16 + 0.4275 * temp * wind ** 0.16).toFixed(1);
+   // Weather Summary
+const apiURL_weather = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=b1c4009595630e2651d3712160a3c1f6&units=imperial';
+
+fetch(apiURL_weather)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+
+        let temp = jsonObject.main.temp;
+        let tempHigh = jsonObject.main.temp_max;
+        let windspeed = jsonObject.wind.speed;
+
+        document.getElementById('currently').textContent = jsonObject.weather[0].description;
+        document.getElementById('temp').textContent = Math.round(temp);
+        document.getElementById('humidity').textContent = jsonObject.main.humidity;
+        document.getElementById('windspeed').textContent = Math.round(windspeed);
+
+        //windchill
+        
+        let output = "N/A"
+        if (temp <= 50 && windspeed > 3) {
+            output = Math.round(35.74 + 0.6215 * temp - 35.75 * windspeed ** 0.16 + 0.4275 * temp * windspeed ** 0.16);
             output += " \xB0F";
         }
 
         document.getElementById("windchill").textContent = output;
 
     });
-
 
 // 5 day forecast
 
@@ -46,4 +64,4 @@ fetch(apiURL_forecast)
   day++;	  
   });
 });
-
+ })
